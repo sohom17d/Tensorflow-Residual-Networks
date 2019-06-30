923d1b74945f5fe22e7ca390654a15df37a8f867
qwerty123456 <19soumik.rakshit96@gmail.com>
2019-06-30 20:25:16 +0530

    Added utility for Convolutional Block

--- models/resnet_utils.py
+++ models/resnet_utils.py
@@ -4,7 +4,7 @@ from tensorflow.keras.initializers import glorot_uniform
 
 
 
-def main_path_block(x, filters, kernel_size, padding, conv_name, bn_name, activation):
+def main_path_block(x, filters, kernel_size, padding, conv_name, bn_name, activation = None, strides = (1, 1)):
     """
     Implementation of a main path block
 
@@ -23,7 +23,7 @@ def main_path_block(x, filters, kernel_size, padding, conv_name, bn_name, activa
     x = Conv2D(
         filters = filters,
         kernel_size = kernel_size,
-        strides = (1, 1),
+        strides = strides,
         padding = padding,
         name = conv_name,
         kernel_initializer = glorot_uniform(seed = 0)
@@ -52,9 +52,10 @@ def identity_block(input_tensor, middle_filter_size, filters, stage, block):
     block ->                string;         Denotes name of the block
 
     Output:
-    returns ->              tensor;         Output Tensor (n, h, w, c)   
+    returns ->              tensor;         Output Tensor (h, w, c)   
     """
 
+    # Set the naming convention
     conv_name_base = 'res' + str(stage) + block + '_branch'
     batch_norm_name_base = 'batch_norm' + str(stage) + block + '_branch'
 
@@ -85,7 +86,7 @@ def identity_block(input_tensor, middle_filter_size, filters, stage, block):
         'relu'
     )
 
-    # Third Block of main path
+    # Last Block of main path
     x = main_path_block(
         x,
         filters[2],
@@ -104,3 +105,81 @@ def identity_block(input_tensor, middle_filter_size, filters, stage, block):
 
 
 
+def convolutional_block(input_tensor, middle_filter_size, filters, stage, block, stride = 2):
+    """
+    Implementation of Identity Block
+
+    Arguments:
+    input_tensor ->         tensor;         Input Tensor (n, h, w, c)
+    middle_filter_size ->   integer;        Size of filter in the middle convolutional block
+    filters ->              integer list;   Number of filters in the convolutional blocks
+    stage ->                integer;        Denotes position of the block in the network
+    block ->                string;         Denotes name of the block
+    stride ->               integer;        Stride in the convolutional layer
+
+    Output:
+    returns ->              tensor;         Output Tensor (h, w, c)   
+    """
+    
+    # Set the naming convention
+    conv_name_base = 'res' + str(stage) + block + '_branch'
+    batch_norm_name_base = 'batch_norm' + str(stage) + block + '_branch'
+
+    x_shortcut = input_tensor
+
+    ## MAIN PATH ##
+
+    # First Block of Main Path
+    x = main_path_block(
+        input_tensor,
+        filters[0],
+        (1, 1),
+        'valid',
+        conv_name_base + '2a',
+        batch_norm_name_base + '2a',
+        'relu',
+        (stride, stride)
+    )
+
+    # Middle Block of main path
+    x = main_path_block(
+        x,
+        filters[1],
+        (
+            middle_filter_size,
+            middle_filter_size
+        ),
+        'same',
+        conv_name_base + '2b',
+        batch_norm_name_base + '2b',
+        'relu'
+    )
+
+    # Last Block of main path
+    x = main_path_block(
+        x,
+        filters[2],
+        (1, 1),
+        'valid',
+        conv_name_base + '2c',
+        batch_norm_name_base + '2c',
+        None
+    )
+
+    ## Skip Connection Convolutional Block ##
+    x_shortcut = main_path_block(
+        x_shortcut,
+        filters[2],
+        (1, 1),
+        'valid',
+        conv_name_base + '1',
+        batch_norm_name_base + '1',
+        None,
+        (stride, stride)
+    )
+
+    # Skip Connection
+    x = Add()([x, x_shortcut])
+    x = Activation('relu')(x)
+
+    return x
