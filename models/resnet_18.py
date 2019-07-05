18a594eb217ac3db7741b2822c8e45eeefc43f9f
Sohom Dey <sohom17d@gmail.com>
2019-07-06 00:02:47 +0530

    Fixed all bugs

--- models/resnet_18.py
+++ models/resnet_18.py
@@ -1,9 +1,6 @@
-from tensorflow.keras.layers import Conv2D, BatchNormalization, Activation, Add
-from tensorflow.keras.layers import Input, ZeroPadding2D, MaxPooling2D, Dropout, concatenate
-from tensorflow.keras.layers import AveragePooling2D, Flatten, Dense
+from tensorflow.keras.layers import Input, MaxPooling2D, AveragePooling2D, Conv2D, BatchNormalization, Activation, Add, Flatten, Dense
 from tensorflow.keras.models import Model
 from tensorflow.keras.initializers import glorot_uniform
-
 
 class Resnet18:
 
@@ -11,7 +8,6 @@ class Resnet18:
     def __init__(self, input_shape, classes):
         """
         Constructor of Resnet50
-
         Arguments:
         input_shape ->      tuple;      Shape of input tensor
         classes     ->      integer;    Number of classes
@@ -24,7 +20,6 @@ class Resnet18:
     def main_path_block(self, x, filters, kernel_size, padding, conv_name, bn_name, batch_norm = True, activation = None, strides = (1, 1)):
         """
         Implementation of a main path block
-
         Arguments:
         x ->            tensor;     Input Tensor
         filters ->      integer;    Number of filters in the convolutional layer
@@ -34,7 +29,6 @@ class Resnet18:
         batch_norm ->   boolean;    Wheather to apply batch normalization or not
         activation ->   string;     Activation to be applied
         strides ->      tuple;      Convolutional Strides
-
         Output:
         returns ->      tensor;     Downsampled tensor
         """
@@ -61,18 +55,16 @@ class Resnet18:
 
 
 
-    def identity_block(self, input_tensor, filters, activation, stage, block, start_with_batch_norm = True):
+    def identity_block(self, input_tensor, filters, activations, stage, block, start_with_batch_norm = True):
         """
         Implementation of Identity Block
-
         Arguments:
         input_tensor ->         tensor;         Input Tensor (n, h, w, c)
-        filters ->              integer;        Number of filters in the convolutional blocks
+        filters ->              integer list;   Number of filters in the convolutional blocks
         activations ->          string list;    List of Activations
         stage ->                integer;        Denotes position of the block in the network
         block ->                string;         Denotes name of the block
         start_with_batch_norm-> boolean;        Whether to start the block with Batchnormalization or not
-
         Output:
         returns ->              tensor;         Output Tensor (h, w, c)   
         """
@@ -80,15 +72,24 @@ class Resnet18:
         # Set the naming convention
         conv_name_base = 'res' + str(stage) + block + '_branch'
         batch_norm_name_base = 'batch_norm' + str(stage) + block + '_branch'
+        
+        if(type(filters) == int):
+          filter_a = filters
+          filter_b = filters
+          
+        else:
+          filter_a = filters[0]
+          filter_b = filters[1]
 
         x_shortcut = input_tensor
-        x = None
+        x = input_tensor
 
         if start_with_batch_norm:
             # Start with Batch Normalization
-            x = BatchNormalization(axis = 3, name = batch_norm_name_base + '_1')(input_tensor)
+            x = BatchNormalization(axis = 3, name = batch_norm_name_base + '_1')(x)
 
-            x = Activation(activation)(x)
+            if activations != None:
+                x = Activation(activations)(x)
         
         else:
             x = input_tensor
@@ -96,18 +97,20 @@ class Resnet18:
         # Main Path Block a
         x = self.main_path_block(
             x,
-            filters,
+            filter_a,
             (3, 3),
             'same',
             conv_name_base + '2a',
             batch_norm_name_base + '2a',
-            activation = activation
+            activation = 'relu'
         )
 
         # Main Path Block b
         x = self.main_path_block(
-            x, filters,
-            (3, 3), 'same',
+            x, 
+            filter_b,
+            (3, 3), 
+            'same',
             conv_name_base + '2b',
             batch_norm_name_base + '2b',
             batch_norm = False,
@@ -120,17 +123,15 @@ class Resnet18:
         return x
     
 
-    def convolutional_block(self, input_tensor, filters, activation, stage, block, start_with_batch_norm = True):
+    def convolutional_block(self, input_tensor, filters, activations, stage, block, start_with_batch_norm = True):
         """
         Implementation of Convolutional Block
-
         Arguments:
         input_tensor ->         tensor;         Input Tensor (n, h, w, c)
         filters ->              integer list;   Number of filters in the convolutional blocks
         stage ->                integer;        Denotes position of the block in the network
         block ->                string;         Denotes name of the block
         stride ->               integer;        Stride in the convolutional layer
-
         Output:
         returns ->              tensor;         Output Tensor (h, w, c)   
         """
@@ -140,46 +141,53 @@ class Resnet18:
         batch_norm_name_base = 'batch_norm' + str(stage) + block + '_branch'
 
         x_shortcut = input_tensor
-        x = None
+        x = input_tensor
 
         ## MAIN PATH ##
 
         if start_with_batch_norm:
             # Start with Batch Normalization
-            x = BatchNormalization(name = batch_norm_name_base + '_1')(input_tensor)
+            x = BatchNormalization(name = batch_norm_name_base + '_1')(x)
 
-            x = Activation(activation)(x)
+            if activations != None:
+                x = Activation(activations)(x)
         
         else:
             x = input_tensor
         
         # Main path block a
         x = self.main_path_block(
-            x, filters[0],
-            (3, 3), 'same',
+            x, 
+            filters[0],
+            (3, 3), 
+            'same',
             conv_name_base + '2a',
             batch_norm_name_base + '2a',
-            activation = activation,
-            strides = (2, 2)
+            activation = 'relu',
+            strides = (1, 1)
         )
 
         # Main path block b
         x = self.main_path_block(
-            x, filters[1],
-            (3, 3), 'same',
+            x, 
+            filters[1],
+            (3, 3), 
+            'same',
             conv_name_base + '2b',
             batch_norm_name_base + '2b',
             batch_norm = False,
             activation = None
         )
-
+        
         ## SKIP PATH ##
 
         # Convolutional Block
         x_shortcut = self.main_path_block(
-            x_shortcut, filters[2],
-            (3, 3), 'same',
-            conv_name_base + '2b',
+            x_shortcut, 
+            filters[2],
+            (3, 3), 
+            'same',
+            conv_name_base + '3b',
             batch_norm_name_base + '2b',
             batch_norm = False,
             activation = None
@@ -222,19 +230,19 @@ class Resnet18:
 
         # Stage 5
         x = self.convolutional_block(x, [512, 512, 512], 'relu', 5, 'a')
-        x = self.identity_block(x, 512, 'relu', 4, 'b')
+        x = self.identity_block(x, 512, 'relu', 5, 'b')
 
         # Fully Connected Layers
         x = BatchNormalization(axis = 3)(x)
         x = Activation('relu')(x)
         x = AveragePooling2D((2, 1), padding = 'valid', strides = (2, 2))(x)
         x = Flatten()(x)
-        x = Dense(512)
+        x = Dense(512)(x)
         x = Dense(
-            self.classes, activation = 'softmax',
+            self.classes,
+            activation = 'softmax',
             name = 'fc_' + str(self.classes),
-            kernel_initializer = glorot_uniform(seed = 0)
-        )(x)
+            kernel_initializer = glorot_uniform(seed = 0))(x)
 
         self.model = Model(input_placeholder, x, name = 'Resnet18')
     
